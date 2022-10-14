1c77ebb32fa917ab7fc59ded71997f8f685d90d5
abhishek986873 <110660543+abhishek986873@users.noreply.github.com>
2022-10-14 10:38:51 +0530

    Add files via upload
    my first java program!

---
+++ percentagecalculator.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+import java.util.Scanner;
+
+public class percentagecalculator {
+    public static void main(String[] args) {
+    Scanner permittotakeinput=new Scanner(System.in); // taking permission to use powers of jaadu
+    // taking input of marks from user
+        System.out.print("Enter Maths Mark = ");
+        int mark1=permittotakeinput.nextInt();
+        System.out.print("Enter Physics Mark = ");
+        int mark2=permittotakeinput.nextInt();
+        System.out.print("Enter Chemistry Mark = ");
+        int mark3=permittotakeinput.nextInt();
+        System.out.print("Enter English Mark = ");
+        int mark4=permittotakeinput.nextInt();
+        System.out.print("Enter IP Mark = ");
+        int mark5=permittotakeinput.nextInt();
+        // adding the marks of all subject
+        int sum=mark1+mark2+mark3+mark4+mark5;
+        float divide=sum/5;
+       // int percentage=divide*100;
+        System.out.print("Percentage = ");
+        System.out.println(divide+"%");
+
+
+
+    }
+
+}
